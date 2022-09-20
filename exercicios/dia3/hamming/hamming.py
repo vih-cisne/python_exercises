e9bc5690c31269fedcf87e6ee5757f49a1e9ef8c
vih-cisne <anavitoriacisn314@gmail.com>
2022-09-20 10:37:08 -0300

    feat: hamming

--- exercicios/dia3/hamming/hamming.py
+++ exercicios/dia3/hamming/hamming.py
@@ -1,2 +1,13 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) == len(strand_b):
+        distance = 0
+
+        for index in range(len(strand_a)):
+            if strand_a[index] != strand_b[index]:
+                distance += 1
+        
+        return distance
+
+    else:
+        raise ValueError("Strands must be of equal length.")
+
